6aaa5f055987ab250ddb7f0c557eb404a528997f
Shane Schilling <trilambda122@gmail.com>
2021-12-13 15:36:49 -0600

    artist count function completed

---
+++ api/count-artist.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk')
+AWS.config.update({region: 'us-east-1'})
+// const mongoose = require('mongoose')
+const showEvent = require('../models/event')
+const utils = require('../utils/utils')
+const connectDatabase = require('../utils/databaseConnect')
+
+exports.handler = async(event,context)=>{
+    context.callbackWaitsForEmptyEventLoop = false;
+    
+    try{
+        const db = await connectDatabase.connect();
+    
+        const result = await showEvent.count().distinct('artist');
+      
+        return {
+            statusCode: 200,
+            headers: utils.getResponseHeaders(),
+            body: JSON.stringify(result.length)
+        }
+    }catch(err){
+        console.log("Error", err)
+        return {
+            statusCode: err.statusCode ? err.statusCode : 500,
+            headers: utils.getResponseHeaders(),
+            body: JSON.stringify({
+                error: err.name ? err.name : "Expection!",
+                message: err.message ? err.message : "UNKNOWN Error"
+            })
+        }
+    }
+}
